4c0ee99254ddea504a7d7a6efe5697bc4747e5bb
yulescrob <122058516+yulescrob@users.noreply.github.com>
2023-02-05 21:47:57 -0500

    Completed project

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        
+    double fallMaintCost = 430.25;
+    double winterMaintCost = 230.50;
+    double springMaintCost = 340.77;
+    double summerMaintCost = 237.75;
+    double yearlyMaintCost;
+    yearlyMaintCost = fallMaintCost + winterMaintCost + springMaintCost + summerMaintCost;
+        System.out.println("The maintenance cost for fall, winter, spring, and summer consecutively is " + fallMaintCost + ", " + winterMaintCost + ", " + springMaintCost + ", " + summerMaintCost + ". The yearly maintenance cost is " + yearlyMaintCost );
 
 
 
